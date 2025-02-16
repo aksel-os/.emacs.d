05191466ddfbe6d2263c9f78001f75864117a220
Aksel Steen - Mac <akselolav@gmail.com>
2025-02-16 15:11:57 +0100

    initial commit

---
+++ init.el
@@ -0,0 +1,12 @@
+;; NOTE: This file will replace it self
+
+;; We need org in order to make use of the tangling functionality
+(require 'org)
+;; Open the org-mode configuration
+(find-file (concat user-emacs-directory "init.org"))
+;; Tangle the file
+(org-babel-tangle)
+;; Load the tangled file
+(load-file (concat user-emacs-directory "init.el"))
+;; Byte-compile it
+(byte-compile-file (concat user-emacs-directory "init.el"))
